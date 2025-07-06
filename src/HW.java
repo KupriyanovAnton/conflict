ff9e8384a5cd5a47720ddc9a59514223a9e3d521
Anton <kuprianov.anton@yandex.ru>
2025-07-06 16:53:10 +0400

    git ignore fix

--- src/HW.java
+++ src/HW.java
@@ -2,4 +2,8 @@ public class HW {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public void doFromFirst() {
+        System.out.println("First");
+    }
 }
