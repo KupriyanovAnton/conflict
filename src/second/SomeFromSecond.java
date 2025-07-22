bfeddce99080b5bf8a4399f136811a4063a4ef5e
Anton <kuprianov.anton@yandex.ru>
2025-07-22 18:34:24 +0400

    from master

--- src/second/SomeFromSecond.java
+++ src/second/SomeFromSecond.java
@@ -3,6 +3,10 @@ package second;
 public class SomeFromSecond {
 
     public void sayHello() {
+        extractedFromMaster();
+    }
+
+    private void extractedFromMaster() {
         System.out.println("Hello Vietnam");
     }
 }
